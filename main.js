3925977ed80ee7cabc5e74281936763403d508cf
npv0710 <namphamviet0710@gmail.com>
2023-06-02 14:02:39 +0700

    check commit to create sub brank

--- main.js
+++ main.js
@@ -219,8 +219,8 @@ $(document).ready(function (){
                     targets: 1,
                     width: 150,
                     render: function(data, type, row, meta) {
-                        return '<td>' +
-                                '<div class="license-plate">' + data + '</div>' +
+                        return '<td class="license-plate">' +
+                                    data + 
                                 '</td>';
                     }
                 }
@@ -239,22 +239,21 @@ $(document).ready(function (){
             "ordering": false,
             "select": true,
             pageLength: 5
+
         })
+
+        $('#table_drivers tbody').on('click', 'tr', function () {
+            var data = tableDriver.row(this).data();
+            $('#title_list_trips').text('Dannh sách chuyến theo biển số xe: ' + data[1])
+            $('#modal_list_trips').modal('show')
+        });
     }
 
-    $(document).on('click', 'td .license-plate', function(e){
+    $(document).on('click', 'td.license-plate', function(e){
         e.preventDefault()
-
-        let index = parseInt($(this).parents('tr').find('td').eq(0).html())
-
-        let driverData = tableDriver.row(index).data()
-
-        console.log(driverData);
-        $('#title_list_trips').text('Dannh sách chuyến theo biển số xe: ' + driverData[1])
-
-        $('#modal_list_trips').modal('show')
-
     })
+
+    
 
     loadGoogleMAP();
 
